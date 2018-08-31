6bef0d86794f8d06af2d50d2c84b1822f1975c58
Olaf Kasper <ziomek88@gmail.com>
2018-08-31 15:53:42 +0200

    Fix tuple formatting (#48)
    * Fix tuple formatting
    
    * v0.5.1-y.14

---
+++ packages/rx-web3/methods/request/formatters.spec.ts
@@ -0,0 +1,28 @@
+import { formatArg } from './formatters';
+import { expect } from 'chai';
+import { BigNumber } from 'bignumber.js';
+
+describe('argument formatting', () => {
+  it('format array', () => {
+    const stringArray = [1, '2', new BigNumber(3)];
+    const expectedStringArray = ['1', '2', '3'];
+    expect(formatArg('bytes32[]', stringArray)).to.deep.eq(expectedStringArray);
+  });
+
+  it('formats each array argument', () => {
+    const stringTuple = [1, '2', new BigNumber(3)];
+    const expectedBytesTuple = ['1', '2', '3'];
+    expect(formatArg('bytes32[]', stringTuple)).to.deep.eq(expectedBytesTuple);
+  });
+
+  it('format tuple', () => {
+    const stringTuple = ['a'];
+    expect(formatArg('bytes32[1]', stringTuple)).to.deep.eq(stringTuple);
+  });
+
+  it('formats each tuple argument', () => {
+    const stringTuple = [1, '2', new BigNumber(3)];
+    const expectedBytesTuple = ['1', '2', '3'];
+    expect(formatArg('bytes32[3]', stringTuple)).to.deep.eq(expectedBytesTuple);
+  });
+});
