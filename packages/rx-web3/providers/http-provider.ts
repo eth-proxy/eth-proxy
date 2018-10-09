80964d1f107e4ec26bd2f2f5dc030c456e289639
Piotr Gąsiorowski <gasiorowski.piotr@gmail.com>
2018-10-09 13:27:40 +0200

    Allow to pass custom headers to HttpProvider (#57)

--- packages/rx-web3/providers/http-provider.ts
+++ packages/rx-web3/providers/http-provider.ts
@@ -1,6 +1,6 @@
 import * as HttpProvider from 'web3/lib/web3/httpprovider';
 import { Provider } from '../interfaces';
 
-export const httpProvider = (url?: string): Provider => {
-  return new HttpProvider(url);
+export const httpProvider = (url?: string, customHeaders?: any): Provider => {
+  return new HttpProvider(url, 0, undefined, undefined, customHeaders);
 };
