5a4c46d82208ad33d5bef8735d5ebd242ccf12af
Olaf Kasper <ziomek88@gmail.com>
2019-03-22 12:01:38 +0100

    Curry subscribefrom

--- packages/rpc/subscriptions/subscribe-from.ts
+++ packages/rpc/subscriptions/subscribe-from.ts
@@ -3,6 +3,7 @@ import { Observable, ReplaySubject, from } from 'rxjs';
 import { first, mergeMap, map, mergeAll, tap } from 'rxjs/operators';
 import { subscribe, getLogs, unsubscribe } from '../methods';
 import { fromLog } from '../converters';
+import { curry } from 'ramda';
 
 interface SubscriptionCreated {
   type: 'SubscriptionCreated';
@@ -22,64 +23,68 @@ export const ofType = (type: LifecycleEvent['type']) => (
   lifecycle$: Observable<LifecycleEvent>
 ) => lifecycle$.pipe(first(x => x.type === type));
 
-export function subscribeFrom(
-  provider: Provider,
-  { address, fromBlock }: Omit<FilterObject, 'toBlock'>
-) {
-  return new Observable<Log>(observer => {
-    const lifecycle$ = new ReplaySubject<LifecycleEvent>();
+export const subscribeLogsFrom = curry(
+  (
+    provider: Provider,
+    { address, fromBlock }: Omit<FilterObject, 'toBlock'>
+  ) => {
+    return new Observable<Log>(observer => {
+      const lifecycle$ = new ReplaySubject<LifecycleEvent>();
 
-    let subId: string | null = null;
-    const subId$ = subscribe(provider, {
-      type: 'logs',
-      args: { address }
-    });
-
-    const liveEvent$ = from(subId$).pipe(
-      mergeMap(id => {
-        subId = id;
-        lifecycle$.next({ type: 'SubscriptionCreated' });
-
-        return provider.observe(id).pipe(map(fromLog));
-      }),
-      tap({
-        error: () => {
-          subId = null;
-        }
-      })
-    );
-
-    const pastEvents$ = getLogs(provider, {
-      address,
-      fromBlock
-    });
-
-    const pastSub = lifecycle$
-      .pipe(
-        ofType('SubscriptionCreated'),
-        mergeMap(() => pastEvents$),
-        mergeAll()
-      )
-      .subscribe({
-        next: e => observer.next(e),
-        error: e => observer.error(e),
-        complete: () => lifecycle$.next({ type: 'CatchedUp' })
+      let subId: string | null = null;
+      const subId$ = subscribe(provider, {
+        type: 'logs',
+        args: { address }
       });
 
-    const liveSub = liveEvent$.subscribe({
-      next: e => {
-        lifecycle$.pipe(ofType('CatchedUp')).subscribe(() => observer.next(e));
-      },
-      error: e => observer.error(e)
-    });
+      const liveEvent$ = from(subId$).pipe(
+        mergeMap(id => {
+          subId = id;
+          lifecycle$.next({ type: 'SubscriptionCreated' });
 
-    return () => {
-      if (subId) {
-        unsubscribe(provider, subId);
-      }
-      lifecycle$.complete();
-      pastSub.unsubscribe();
-      liveSub.unsubscribe();
-    };
-  });
-}
+          return provider.observe(id).pipe(map(fromLog));
+        }),
+        tap({
+          error: () => {
+            subId = null;
+          }
+        })
+      );
+
+      const pastEvents$ = getLogs(provider, {
+        address,
+        fromBlock
+      });
+
+      const pastSub = lifecycle$
+        .pipe(
+          ofType('SubscriptionCreated'),
+          mergeMap(() => pastEvents$),
+          mergeAll()
+        )
+        .subscribe({
+          next: e => observer.next(e),
+          error: e => observer.error(e),
+          complete: () => lifecycle$.next({ type: 'CatchedUp' })
+        });
+
+      const liveSub = liveEvent$.subscribe({
+        next: e => {
+          lifecycle$
+            .pipe(ofType('CatchedUp'))
+            .subscribe(() => observer.next(e));
+        },
+        error: e => observer.error(e)
+      });
+
+      return () => {
+        if (subId) {
+          unsubscribe(provider, subId);
+        }
+        lifecycle$.complete();
+        pastSub.unsubscribe();
+        liveSub.unsubscribe();
+      };
+    });
+  }
+);
